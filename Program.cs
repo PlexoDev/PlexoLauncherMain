00831dfac0f731dfdcbfda5b816c2b0c35b43e79
p0s0 <73150246+p0s0@users.noreply.github.com>
2022-06-17 20:23:08 -0400

    Add generateBaseUrl(int type)

--- Program.cs
+++ Program.cs
@@ -8,6 +8,21 @@ namespace PlexoLauncherMain
 {
     class Program
     {
+        private static string currentVersion = "version-b017cc18d6f77d3d";
+        private static string generateBaseUrl(int type)
+        {
+            switch(type)
+            {
+                case 0: // plexxo.xyz
+                    return "http://plexxo.xyz";
+                case 1: // setup.plexxo.xyz
+                    return "http://setup.plexxo.xyz";
+                case 2: // api.plexxo.xyz
+                    return "http://api.plexxo.xyz";
+                default:
+                    return "http://plexxo.xyz";
+            }
+        }
         static void Main(string[] args)
         {
         }
