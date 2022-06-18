abc4b82d5c2d82a21f9c7f9193b7b357c22a1991
p0s0 <73150246+p0s0@users.noreply.github.com>
2022-06-17 23:09:43 -0400

    hehehe

--- Program.cs
+++ Program.cs
@@ -1,14 +1,32 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Win32;
 
 namespace PlexoLauncherMain
 {
     class Program
     {
-        private static string currentVersion = "version-b017cc18d6f77d3d";
+        private static string currentVersion = ""; // we get this later on
+        private static string tempPath = Path.GetTempPath();
+        private static string getStringFromUrl(string url)
+        {
+            using(WebClient wc = new WebClient())
+            {
+                return wc.DownloadString(url);
+            }
+        }
+        private static void downloadFile(string url, string location)
+        {
+            using(WebClient wc = new WebClient())
+            {
+                wc.DownloadFile(url, location);
+            }
+        }
         private static string generateBaseUrl(int type)
         {
             switch(type)
@@ -19,12 +37,15 @@ namespace PlexoLauncherMain
                     return "http://setup.plexxo.xyz";
                 case 2: // api.plexxo.xyz
                     return "http://api.plexxo.xyz";
-                default:
+                default: // plexxo.xyz
                     return "http://plexxo.xyz";
             }
         }
         static void Main(string[] args)
         {
+            currentVersion = getStringFromUrl(generateBaseUrl(1) + "/version");
+            downloadFile(generateBaseUrl(1) + "/Roblox.exe", tempPath);
+            // :troll:
         }
     }
 }
